c01a0b756dfc903d2a10a46ac658961c166821a5
gackermannlogan <60230277+gackermannlogan@users.noreply.github.com>
2020-04-12 22:08:28 -0400

    Update KMeansClustering_functions.py

--- KMeansClustering_functions.py
+++ KMeansClustering_functions.py
@@ -1,3 +1,4 @@
+
 #Project 1 Step 4
 #Name: Gaby Ackermann Logan 
 
@@ -19,14 +20,14 @@ def normalize(glucose, hemolobin, classification):
 #glucose_scaled, and hemoglobin_scaled
     g = []
     h = []
-    for i in glucose:
-        newg = (i-70)/(490-70)
+    for l in glucose:
+        newg = (l-70)/(490-70)
         g.append(newg)
-    for i in hemoglobin:
-        newh = (i- 3.1)/(17.8-3.1)
+    for l in hemoglobin:
+        newh = (l- 3.1)/(17.8-3.1)
         h.append(newh)
-    hemoglobin_scaled = np.array(h)
     glucose_scaled = np.array(g)
+    hemoglobin_scaled = np.array(h)
     classification = np.array(classification)
     return glucose_scaled, hemoglobin_scaled, classification
 
@@ -39,49 +40,51 @@ def generate_centroids(k):
 # scaled centroids
     centroids_generated = []
     for i in range (k):
-        g = random.uniform (0.0, 1.0)
-        h = random.uniform(0.0, 1.0)
+        g = random.uniform (0,1)
+        h = random.uniform(0,1)
         centroids_generated.append([g,h,i])
     newcentroids = np.array(centroids_generated)
     return newcentroids
 
-def assign(newcentroids, hemoglobin_scaled, glucose_scaled):
-    K = newcentroids.shape[0]
-    distances = np.zeros((K, len(hemoglobin)))
-    for i in range(K):
-        new_glucose = glucose[i]
-        new_hemoglobin = hemoglobin[i]
-        distances= calculateDistanceArray(K, new_glucose, new_hemoglobin)
-        assignments = np.argmin(distances, axis = 0)    
-        c = assignments[2]
-        assign.append([c,new_glucose,new_hemoglobin])
-    final_assignment = np.array(assign)
-    return final_assignment
+def calculateDistanceArray(newcentroids, glucose_scaled, hemoglobin_scaled):
+#This function calculates the distance between the scaled hemoglobin and glucose points
+#and each centroid and then returns the calculatted distances. It takes the
+#new array of centroids and the scaled hemolgobin and glucose as parameters
+    newdistance = []
+    for i in range(len(newcentroids)):
+        centroid_array = newcentroids[i]
+        distance = np.sqrt(((centroid_array[0]-glucose_scaled)**2) +((centroid_array[1]-hemoglobin_scaled)**2))
+        newdistance.append(distance)
+    distance_array = np.array(newdistance)
+    return distance_array
 
-def calculateDistanceArray(newcentroids, glucose, hemoglobin):
-    d = []
-    for i in range(0,159):
-        distance= np.sqrt(((newcentroids[0]-glucose)**2) +((newcentroids[1]-hemoglobin)**2))
-        d.append(distance)
-    finaldistance = np.array(d)
-    return finaldistance
+def clustering(newcentroids,hemoglobin_scaled, glucose_scaled):
+#This function assigns and updates the centroids locatins and returns the final 
+#locations and classifications 
+#it takes the the new array of centroids and the scaled hemolgobin and glucose as parameters
+    interations = 0
+    while interations < 100:
+        K = newcentroids.shape[0]
+        distance = np.zeros((K, len(hemoglobin_scaled)))
+        for i in range(K):
+            distance = calculateDistanceArray (newcentroids, glucose_scaled, hemoglobin_scaled)
+        final_assignments = np.argmin(distance, axis = 0)
+        updated_centroids = np.zeros((K,2))
+        assignK = final_assignments.sort()
+        for i in range (K):
+            updated_centroids[i][1]= np.mean(glucose_scaled[assignK==i])
+            updated_centroids[i][0]= np.mean(hemoglobin_scaled[assignK==i])
+        interations +=1
+    return final_assignments, updated_centroids
 
-def update(final_assignment, glucose_scaled, hemoglobin_scaled, newcentroids):
-    K = newcentroids.shape[0]
-    updated_centroids = np.zeros((K,2))
-    assignK = final_assignment.sort()
-    for i in range (0, 159):
-        update_glucose = np.mean(glucose[assignK==i])
-        update_hemoglobin = np.mean(hemoglobin[assignK ==i])
-        updated_centroids[i] = np.append(update_glucose, update_hemoglobin)
-    return updated_centroids
-
-def graphingkMeans(glucose, hemoglobin, assignment,centroids):
+def graphingkMeans(glucose_scaled, hemoglobin_scaled, final_assignments,newcentroids):
+#This fucntion graphs the centroids and the clusters. It has no returns and takes the 
+#scaled hemoglobin, glucose and centroids as parameters
     plt.figure()
-    for i in range(assignment.max()+1):
+    for i in range(len(updated_centroids)):
         rcolor = np.random.rand(3,)
-        plt.plot(hemoglobin[assignment==i],glucose[assignment==i], ".", label = "Class " + str(i), color = rcolor)
-        plt.plot(centroids[i, 0], centroids[i, 1], "D", label = "Centroid " + str(i), color = rcolor)
+        plt.plot(hemoglobin[final_assignments==i],glucose[final_assignments==i], ".", label = "Class " + str(i), color = rcolor)
+        plt.plot(newcentroids[i, 0], newcentroids[i, 1], "D", label = "Centroid " + str(i), color = rcolor)
     plt.xlabel("Hemoglobin")
     plt.ylabel("Glucose")
     plt.legend()
@@ -90,7 +93,6 @@ def graphingkMeans(glucose, hemoglobin, assignment,centroids):
 #Main Script
 glucose, hemoglobin, classification = openckdfile()
 glucose_scaled, hemoglobin_scaled, classification = normalize(glucose, hemoglobin, classification)
-newcentroids = select(5)
-final_assignment = assign(newcentroids, hemoglobin_scaled, glucose_scaled)
-updated_centroids = update(final_assignment, glucose, hemoglobin, newcentroids)
-graphingkMeans(glucose_scaled, hemoglobin_scaled, final_assignment, updated_centroids)
+newcentroids = select(2)
+final_assignments,updated_centroids = clustering(newcentroids,hemoglobin_scaled, glucose_scaled)
+graphingkMeans(glucose_scaled, hemoglobin_scaled, final_assignments, updated_centroids)
